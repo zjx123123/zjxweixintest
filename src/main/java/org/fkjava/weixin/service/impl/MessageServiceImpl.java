ccafff4ace7e96d97f3b5b59ce5c30fa91dc15f6
Administrator <Administrator@PC-20171122QSDS>
2019-04-04 13:31:44 +0800

    aaa

--- src/main/java/org/fkjava/weixin/service/impl/MessageServiceImpl.java
+++ src/main/java/org/fkjava/weixin/service/impl/MessageServiceImpl.java
@@ -29,8 +29,8 @@ public class MessageServiceImpl implements MessageService {
 		// 不同的消息由于处理方式不同，所以建议放入不同的通道中，队列里面可以有近乎无数的通道。
 		// 如果多人共享一个服务器，还需要在通道前面增加一个特征。kemao_3_就是特征。
 		// msg.getMsgType()把不同类型的消息放入不同通道。
-		inMessageTemplate.convertAndSend("wjq_" + msg.getMsgType(), msg);
-
+		inMessageTemplate.convertAndSend("zjx_" + msg.getMsgType(), msg);
+ 
 		// 返回的信息先不管
 		return null;
 	}
